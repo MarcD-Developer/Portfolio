d81322c749a95e10a67d9547e7d42ce8e0a6bbcb
MarcD-Developer <msdilinila@gmail.com>
2024-12-12 15:44:47 -0500

    Updated project to include correct tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./index.html"],
   theme: {
     extend: {},
   },
